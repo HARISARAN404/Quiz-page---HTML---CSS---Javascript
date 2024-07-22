ab3af2d78d032dfb81188e55c25373b24594fbb6
HARISARAN405 <113167317+HARISARAN405@users.noreply.github.com>
2024-07-22 16:21:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function change(th,option){
+    let choose = 'D';
+    if(choose===option){
+        th.style.backgroundColor="green";
+    }
+    else{
+        th.style.backgroundColor="red";
+    }
+}
